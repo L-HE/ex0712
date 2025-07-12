2d36b1e02e1767d175781f8623aa331f6f8dcb24
LHE <mekakusira@gmail.com>
2025-07-13 00:40:39 +0900

    Main, area add

---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+function Main() {
+    return ( 
+        <>
+            <NavLink to='/map'>A구역</NavLink>
+        </>
+     );
+}
+
+export default Main;
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from 'react-router';
 import { lazy, Suspense } from 'react';
 
 const Loading = () => <div>Loading...</div>
+const Main = lazy(() => import("../pages/Main"))
 const FactoryMap = lazy(() => import("../pages/FactoryMap"))
 const Detail = lazy(() => import("../pages/MachineDetailPage"))
 
 const router = createBrowserRouter([
     {
         path: "/",
+        element: <Suspense fallback={<Loading/>}><Main/></Suspense>,
+    },
+    {
+        path: "/map",
         element: <Suspense fallback={<Loading/>}><FactoryMap/></Suspense>,
     },
     {
